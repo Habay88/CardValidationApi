bb1f319b4f963aae90513e0bf5776980fbaa2167
Habay <biodun.banjoko@yahoo.com>
2023-12-23 13:19:52 +0100

    cardschemecontroller

--- src/main/java/com/mintyn/controller/CardSchemeController.java
+++ src/main/java/com/mintyn/controller/CardSchemeController.java
@@ -1,46 +1,29 @@
 package com.mintyn.controller;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import com.mintyn.model.CardVerificationResponse;
+import com.mintyn.service.BinService;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class CardSchemeController {
 
-    private final WebClient webClient;
+    private final BinService binService;
 
-    public CardSchemeController(WebClient.Builder webClientBuilder,
-                                @Value("${binlist.api.url}") String binListApiUrl) {
-        this.webClient = webClientBuilder.baseUrl(binListApiUrl).build();
+    @Autowired
+    public CardSchemeController(BinService binService) {
+        this.binService = binService;
     }
 
-    @GetMapping("/card-scheme/verify/{cardNumber}")
-    @ResponseBody
-    public CardVerificationResponse verifyCard(@PathVariable String cardNumber) {
-        // Use WebClient to make a GET request to the external API
-        BinListResponse binListResponse = webClient.get()
-                .uri("/{cardNumber}", cardNumber)
-                .retrieve()
-                .bodyToMono(BinListResponse.class)
-                .block(); // blocking for simplicity, consider using reactive programming for non-blocking
-
-        // Map the external API response to your custom response
-        return new CardVerificationResponse(
-                true,
-                new CardPayload(
-                        binListResponse.getScheme(),
-                        binListResponse.getType(),
-                        binListResponse.getBank()
-                )
-        );
+    @GetMapping("/card/details/{cardNumber}")
+    public Mono<String> getCardDetails(@PathVariable String cardNumber) {
+        return binService.getCardDetails(cardNumber)
+                .map(response -> "Card is valid\n" +
+                        "Scheme: " + response.getScheme() + "\n" +
+                        "Bank: " + response.getBank())
+                .defaultIfEmpty("Card not found or invalid");
     }
-
-    // ... (other methods)
-
 }
